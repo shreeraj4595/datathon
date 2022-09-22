da685a75f2ce694e7c3d3a9c19aba6615afe825d
Shreeraj Bhat <shreeraj.bhat@cactusglobal.com>
2022-09-22 15:54:38 +0530

    Modified the columns required, implemented basemodel and handling error casses

--- app/db.py
+++ app/db.py
@@ -20,18 +20,20 @@ def connect():
         return ""
 
 
-def get_subject_data(limit, offset, where):
-    print(f"get_subject_data {limit}, {offset}, {where}")
+def get_subject_data(limit, offset, where, year):
+    print(f"get_subject_data {limit}, {offset}, {where}, {year}")
     final_data = {}
     cursor = connect()
     if cursor == "":
         return ""
     try:
+        if len(year) == 0:
+            year = ['2022', '2021', '2020', '2019', '2018']
         query = (
-            "select a.top_concepts_0, a.title, a.dates_pub, a.dates_accepted, a.dates_online, p.publisher_name_ui, j.journal_name, a.subject_areas_crossref_0",
+            "select a.dates_pub, a.dates_accepted, a.dates_online, p.publisher_name_ui, a.subject_areas_crossref_0, date_part('year', a.dates_pub) as pub_year",
             "from Artifacts a "
             "inner join publisher p on a.publisher_id = p.publisher_id inner join Journal j on a.journal_id=j.journal_id "
-            "where date_part('year', a.dates_pub) in ('2022', '2021', '2020', '2019', '2018', '2017') "
+            "where date_part('year', a.dates_pub) in (" + ", ".join(year) + ") "
             "and j.journal_name is not NULL and a.dates_online is not NULL and a.dates_accepted is not NULL "
             "and a.dates_accepted is not NULL and a.subject_areas_crossref_0 LIKE '"
             + where.capitalize()
@@ -45,18 +47,16 @@ def get_subject_data(limit, offset, where):
         index = 0
         for row in subject_records:
             return_list = {}
-            return_list["paper"] = row[0]
-            return_list["title"] = row[1]
-            return_list["dates_pub"] = row[2]
-            return_list["dates_accepted"] = row[3]
-            return_list["dates_online"] = row[4]
-            return_list["publisher_name_ui"] = row[5]
-            return_list["journal_name"] = row[6]
-            return_list["subject_areas_crossref_0"] = row[7]
-            return_list["dates_publ_minus_accepted_days"] = (row[2] - row[3]).days
+            # return_list["dates_pub"] = row[0]
+            # return_list["dates_accepted"] = row[1]
+            # return_list["dates_online"] = row[2]
+            return_list["publisher_name_ui"] = row[3]
+            return_list["subject_areas_crossref_0"] = row[4]
+            return_list["dates_publ_minus_accepted_days"] = (row[0] - row[1]).days
             return_list["dates_publ_minus_accepted_month"] = (
-                round(int((row[2] - row[3]).days) / 30, 2)
+                round(int((row[0] - row[1]).days) / 30, 2)
             )
+            return_list["pub_year"] = row[5]
             final_data[index] = return_list
             index = index + 1
 
@@ -102,18 +102,20 @@ def get_subject_data_list(limit, offset):
             print("PostgreSQL connection is closed")
 
 
-def get_country_data(limit, offset, where):
-    print(f"get_country_data {limit}, {offset}, {where}")
+def get_country_data(limit, offset, where, year):
+    print(f"get_country_data {limit}, {offset}, {where}, {year}")
     final_data = {}
     cursor = connect()
     if cursor == "":
         return ""
     try:
+        if len(year) == 0:
+            year = ['2022', '2021', '2020', '2019', '2018']
         query = (
             "select a.top_concepts_0, a.title, a.dates_pub, a.dates_accepted, a.dates_online, auth.authors_0_aff_address_country ",
             "from Artifacts a "        
             "inner join authors auth on a.dyson_id = auth.dyson_id "
-            "where date_part('year', a.dates_pub) in ('2022', '2021', '2020', '2019', '2018', '2017') "
+            "where date_part('year', a.dates_pub) in (" + ", ".join(year) + ") "
             "and a.dates_online is not NULL and a.dates_accepted is not NULL "
             "and a.dates_accepted is not NULL and auth.authors_0_aff_address_country is not NULL and auth.authors_0_aff_address_country LIKE '"
             + where.capitalize()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from typing import List
 from fastapi import FastAPI, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from pydantic import BaseModel
+from typing import Optional, List
 import es
 import db
 
@@ -14,6 +17,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class DataModelIn(BaseModel):
+    limit: int = 100
+    sub_area: str = ""
+    skip: int = 0
+    country: str = ""
+    year: List[str] = []
+
 @app.get("/getPublishedDate")
 async def get_date_difference():
     '''
@@ -22,22 +32,19 @@ async def get_date_difference():
     return es.get_dates_diff()
 
 
-@app.get("/getSubjectAreaData", status_code=200)
-async def get_subject_data(limit: int = 100, sub_area: str = "", skip: int = 0):
+@app.post("/getSubjectAreaData", status_code=200)
+async def get_subject_data(data: DataModelIn):
     """
     This API is used to query the set of data from Artifacts table.
     limit is default to 100, can be passed other values from the caller
     sub area can be queried for particular value not mandatory
     skip is the offset to specify from what record to query for default to 0
     """
-    ret = db.get_subject_data(limit, skip, sub_area)
+    if data.country != "":
+        raise HTTPException(status_code=400, detail="Year param is invalid in this API")
+    ret = db.get_subject_data(data.limit, data.skip, data.sub_area, data.year)
     if ret == "":
-        # return {
-        #     "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #     "message": "Oops! Something went wrong",
-        # }
         raise HTTPException(status_code=500, detail="Oops! Something went wrong")
-    # print(ret)
     return ret
 
 
@@ -61,24 +68,21 @@ async def get_subject_list(limit: int = 100, skip: int = 0, query_from: str = "d
     # print(ret)
     return ret
 
-@app.get("/getCountryWiseData", status_code=200)
-async def get_country_data(limit: int = 100, country: str = "", skip: int = 0):
+@app.post("/getCountryWiseData", status_code=200)
+async def get_country_data(data: DataModelIn):
     """
     This API is used to query the set of data from authors and artifacts table.
     limit is default to 100, can be passed other values from the caller
     country can be queried for particular value not mandatory
     skip is the offset to specify from what record to query for default to 0
     """
-    ret = db.get_country_data(limit, skip, country)
+    if data.sub_area != "":
+        raise HTTPException(status_code=400, detail="Sub Area param is invalid in this API")
+    ret = db.get_country_data(data.limit, data.skip, data.country, data.year)
     if ret == "":
-        # return {
-        #     "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #     "message": "Oops! Something went wrong",
-        # }
         raise HTTPException(status_code=500, detail="Oops! Something went wrong")
-    # print(ret)
     return ret
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8082)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8082)
