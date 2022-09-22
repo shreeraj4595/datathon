4f4547d5557a535582ebd0b1a968a43daaa193b6
devrasmeriya1012 <devendra.rasmeriya@cactusglobal.com>
2022-09-22 13:08:23 +0530

    minor change

--- app/main.py
+++ app/main.py
@@ -84,24 +84,11 @@ async def get_country_data(limit: int = 100, country: str = "", skip: int = 0):
 #     uvicorn.run(app, host="0.0.0.0", port=8082)
 
 @app.get("/getCountryList", status_code=200)
-async def get_country_list(limit: int = 100, skip: int = 0, query_from: str = "db"):
+async def get_country_list(limit: int = 100, skip: int = 0):
     """
-    This API is used to get the list of subject area. This DB query takes lot of time, use with caution and only if necessary
-    limit is default to 100, can be passed other values from the caller. Give query_from as "es" to query from Elastic Search
-    skip is the offset to specify from what record to query for default to 0
+    This API is used to get the list of Country. 
     """
-    if query_from == "db":
-        ret = db.get_country_list(limit, skip)
-    else:
-        ret = ""
+    ret = db.get_country_list(limit, skip)
     if ret == "":
-        # return {
-        #     "code": status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #     "message": "Oops! Something went wrong",
-        # }
         raise HTTPException(status_code=500, detail="Oops! Something went wrong")
-    # logging.info(ret)
     return ret
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8082)
